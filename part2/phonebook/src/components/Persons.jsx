bc7748b50082ef421d33b3900a47b81dd53fe6c8
m3xy0 <134509199+m3xy0@users.noreply.github.com>
2024-08-09 22:46:24 +0300

    In phonebook app, I fixed an issue about Persons component and revised key values of li items in Persons component from person.name to person.id

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -4,26 +4,12 @@ const Persons = ({persons, filterName, handleDeletion}) => {
     return (
         <ul>
             {filterName === '' 
-            ? persons.map(person => <li key={person.name}>{person.name} {person.number} <button onClick={() => handleDeletion(person.id)}>delete</button></li>)
-            : persons.filter(person => person.name.toLowerCase().includes(filterName.toLowerCase())).map(person => <li key={person.name}>{person.name} {person.number} <button onClick={handleDeletion(person.id)}>delete</button></li>)
+            ? persons.map(person => <li key={person.id}>{person.name} {person.number} <button onClick={() => handleDeletion(person.id)}>delete</button></li>)
+            : persons.filter(person => person.name.toLowerCase().includes(filterName.toLowerCase())).map(person => <li key={person.id}>{person.name} {person.number} <button onClick={() => handleDeletion(person.id)}>delete</button></li>)
             }
         </ul>
     )
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 export default Persons
